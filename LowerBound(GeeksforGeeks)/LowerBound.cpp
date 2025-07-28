6fb70900827a8d48150f639f42d74e665cb59979
Luvin <luvdaku2212@gmail.com>
2025-07-28 08:22:31 +0530

    Add files via upload

---
+++ LowerBound(GeeksforGeeks)/LowerBound.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int lowerBound(vector<int>& arr,int target){
+    int low=0,high=arr.size()-1;
+    int res=arr.size();
+    while(low<=high){
+        int mid=low+((high-low)/2);
+        if(arr[mid]>=target){
+            res=mid;
+            high=mid-1;
+        }else{
+            low=mid+1;
+        }
+        
+    }
+    return res;
+}
